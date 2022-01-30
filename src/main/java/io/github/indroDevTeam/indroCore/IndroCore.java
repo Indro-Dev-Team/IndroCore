89f5db0af51da705c368fa8d1252cfe69bf43ace
skyQuiz <91608035+skyQuiz@users.noreply.github.com>
2022-01-30 18:43:40 +1000

    Some basic templates to save files

---
+++ src/main/java/io/github/indroDevTeam/indroCore/IndroCore.java
@@ -0,0 +1,15 @@
+package io.github.indroDevTeam.indroCore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class IndroCore extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
---
+++ src/main/java/io/github/indroDevTeam/indroCore/utils/YmlUtils.java
@@ -0,0 +1,48 @@
+package io.github.indroDevTeam.indroCore.utils;
+
+import io.github.indroDevTeam.indroCore.IndroCore;
+import org.bukkit.plugin.Plugin;
+
+import java.io.File;
+import java.io.IOException;
+
+public class YmlUtils {
+    private final Plugin plugin;
+    private File file;
+
+    public YmlUtils(Plugin plugin) {
+        this.plugin = plugin;
+    }
+
+    public void createFile(String fileName) {
+        try {
+            File file1 = new File(plugin.getDataFolder().getAbsolutePath() + File.separator + fileName + ".json");
+            if (file1.createNewFile()) {
+                System.out.println("File created: " + file1.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Save a default file saved in a file
+     * @param file The default file saved in a plugin
+     * @param fileName The file that you want to save in it
+     */
+    public void saveDefaultFile(File file, String fileName) {
+        try {
+            if (file.createNewFile()) {
+                System.out.println("File created: " + file.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
